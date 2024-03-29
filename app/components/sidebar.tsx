c315e7df8562a3ec3857a5f4ff8d7f0d7c02beb8
suorui7 <147809740+suorui7@users.noreply.github.com>
2024-03-29 17:24:47 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -158,7 +158,7 @@ export function SideBar(props: { className?: string }) {
           小悟空
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          好好运动、好好学习、好好玩、好好吃、好好休息!
+          好好运动、好好学习、好好玩、好好休息!
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
